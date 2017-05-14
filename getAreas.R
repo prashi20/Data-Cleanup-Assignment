bed2071647bbab52daf1169003ad040e66d916f2
thermoflux <akotha05@gmail.com>
2017-05-14 00:25:24 -0500

    Changed comments to reflect use of imputeTS - Changed comments to reflect use of imputeTS - Formated for better readability

--- getAreas.R
+++ getAreas.R
@@ -9,19 +9,21 @@ dirty_data<-read.csv(fname)
 # Setup for task: Poulate the missing values in the Area variable with an
 # appropriate values (Birmingham, Coventry, Dudley, Sandwell, Solihull, Walsall
 # or Wolverhampton)
-# This requires hot deck imutation method called Last Observed Carried Forward (LOCF) 
+# This requires Impute Time Series library. It provides Last Observed Carried 
+# Forward (LOCF) method.
 library(imputeTS)
-# This library provides Sequential Hot Deck Imputation function, which can be
-# configured to function as LOCF algorithm.
+
 # Input to na.locf() must be a numerix vector. Since the data is in string format
 # a vector is created using factor().
 dirty_data$AreaFactors <- as.integer(factor(dirty_data$Area))
+
 # All "" levels are replaced with 1, so next step is to replace 1 with 'NA'
 dirty_data$AreaFactors[dirty_data$AreaFactors ==1] <- NA
+
 # Impute operation
 dirty_data$AreaFactors<-na.locf(dirty_data$AreaFactors,option = "locf",
                                                   na.remaining = "keep")
-# Convert factors back to levels
+# Convert factors back to levels and write back to dirty_data$Area
 dirty_data$Area<- levels(dirty_data$Area)[dirty_data$AreaFactors]
 
 # Clear temp variables
@@ -32,7 +34,5 @@ fname <- "dirty_data2.csv"
 write.csv(dirty_data, file = fname, eol = "\n", na = "NA",
             row.names = FALSE, fileEncoding = "")
 
+# Read back data 
 dirty_data<-read.csv(fname)
-
-
-
