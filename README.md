c2efda9399a65d0246e1a3ae00a4b4ba7244859c
CynthiaY725 <yangchen0725@gmail.com>
2017-05-16 12:34:15 -0500

    Removed the "strange HTML" Column

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Data-Cleanup-Assignment
 DUE on May 21, 2017
+removed the "Strange.HTML" Column
---
+++ RemoveCol.R
@@ -0,0 +1,10 @@
+
+# Read the dirty data from last step
+mydata<-read.csv("dirty_data2.csv",header = TRUE)
+# Remove the "Strange.HTML" Column
+dirty_data3 = subset(mydata,select = -c(Strange.HTML))
+# Write the updated data into csv file
+write.csv(dirty_data3,file = "dirty_data3.csv")
+
+
+
